8ad2a6918970117b2d3e3f5380f4638c972f35fc
Igor Pugachev <wotwbb@gmail.com>
2023-07-16 20:05:33 +0300

    fix bugs

--- anki-russian-korean.py
+++ anki-russian-korean.py
@@ -14,8 +14,6 @@ CURRENT_DIR = Path(__file__).resolve().parent
 MEDIA_DIR = CURRENT_DIR / 'media'
 MEDIA_DIR.parent.mkdir(parents=True, exist_ok=True)
 
-GOOGLE_CRAWLER = GoogleImageCrawler(storage={'root_dir': MEDIA_DIR}, log_level=logging.CRITICAL)
-
 ANKI_MODEL = genanki.Model(
     1607392319,
     'Gen model',
@@ -66,16 +64,21 @@ def get_translation(word, api_key):
     root = ET.fromstring(res.text)
     translations = root.findall("./item/sense/translation/trans_word")
     if translations == []:
-        raise RuntimeError(f'Не могу найти перевод слова {word}')
+        logging.error(f'Не могу найти перевод слова {word}')
+        return None
     return translations[0].text
 
 def download_image(word, filename):
-    for _ in range(3):
+    for _ in range(5):
         try:
-            GOOGLE_CRAWLER.crawl(keyword=word, max_num=1, overwrite=True)
+            crawler = GoogleImageCrawler(storage={'root_dir': MEDIA_DIR}, log_level=logging.ERROR)
+            crawler.crawl(keyword=word, max_num=1, overwrite=True)
             break
         except:
             continue
+    else:
+        logging.error(f'Не могу найти картинку для слова {word}')
+        return None
     image_file = next(MEDIA_DIR.glob('000001.*'))
     new_name = word_dir(filename) / f'{filename}{image_file.suffix}'
     image_file.rename(new_name)
@@ -90,9 +93,19 @@ def make_note(word, api_key):
     make_word_dir(word)
     sound_file = make_sound(word)
     translation = get_translation(word, api_key)
-    image_file = download_image(translation.split(';')[0], word)
-    note = genanki.Note(model=ANKI_MODEL, fields=[word, translation, f'<img src="{image_file.name}">', f'[sound:{sound_file.name}]'])
-    return (note, [image_file, sound_file])
+    if translation is None:
+        translation = ''
+        image_file = download_image(word, word)
+    else:
+        image_file = download_image(translation.split(';')[0], word)
+    if image_file is None:
+        image_field = ''
+        image_media = []
+    else:
+        image_field = f'<img src="{image_file.name}">'
+        image_media = [image_file]
+    note = genanki.Note(model=ANKI_MODEL, fields=[word, translation, image_field, f'[sound:{sound_file.name}]'])
+    return (note, [sound_file] + image_media)
 
 def read_words_from_file(file_path):
     with open(args.words_list_file, 'r', encoding='UTF-8') as f:
@@ -121,7 +134,6 @@ def make_deck(deck_name, api_key, words):
     package = genanki.Package(deck)
     package.media_files = media
     package.write_to_file(CURRENT_DIR / f'{deck_name}.apkg')
-    shutil.rmtree(MEDIA_DIR)
 
 parser = argparse.ArgumentParser(
     prog='anki-russian-korean', 
@@ -129,6 +141,9 @@ parser = argparse.ArgumentParser(
 parser.add_argument('deck_name', help='Название колоды')
 parser.add_argument('api_key', help='Ключ API krdict')
 parser.add_argument('words_list_file', help='Путь к файлу с корейскими словами по одному слову на строке')
+parser.add_argument('--no_cleanup', action='store_true', help='Не удалять медиа файлы после работы скрипта')
 args = parser.parse_args()
 
 make_deck(args.deck_name, args.api_key, read_words_from_file(args.words_list_file))
+if not args.no_cleanup:
+    shutil.rmtree(MEDIA_DIR)
